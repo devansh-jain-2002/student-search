627d8948e714c64f3fb19766d3b32e6598526499
devansh-jain-2002 <jdevansh21@iitk.ac.in>
2022-12-10 16:57:22 +0530

    Async Scraper

--- scrape/scrape.py
+++ scrape/scrape.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python3
 
 from bs4 import BeautifulSoup
-import re
-import requests
 import sqlite3
+import aiohttp
+import asyncio
 conn = sqlite3.connect('../database/students.db')
 c = conn.cursor()
-
-s = requests.Session()
-s.get("https://oa.cc.iitk.ac.in/Oa/Jsp/Main_Frameset.jsp")
-s.get("https://oa.cc.iitk.ac.in/Oa/Jsp/Main_Intro.jsp?frm='SRCH'")
-s.get("https://oa.cc.iitk.ac.in/Oa/Jsp/OAServices/IITK_Srch.jsp?typ=stud")
 
 headers = {
     "Referer": "https://oa.cc.iitk.ac.in/Oa/Jsp/OAServices/IITK_Srch.jsp?typ=stud"
@@ -19,36 +14,32 @@ headers = {
 headers1 = {
     "Referer": "https://oa.cc.iitk.ac.in/Oa/Jsp/OAServices/IITk_SrchStudRoll_new.jsp"
 }
-
-payload = {
-    'k4': 'oa',
-    'numtxt': '',
-    'recpos': 0,
-    'str': '',
-    'selstudrol': '',
-    'selstuddep': '',
-    'selstudnam': '',
-    'txrollno': '',
-    'Dept_Stud': '',
-    'selnam1': '',
-    'mail': ''
-}
-
-payload1 = {
-    'typ': ['stud'] * 12,
-    'numtxt': '',
-    'sbm': ['Y'] * 12
-}
+def get_payload(i):
+    payload = {
+        'k4': 'oa',
+        'numtxt': '',
+        'recpos': i,
+        'str': '',
+        'selstudrol': '',
+        'selstuddep': '',
+        'selstudnam': '',
+        'txrollno': '',
+        'Dept_Stud': '',
+        'selnam1': '',
+        'mail': ''
+    }
+    return payload
+def get_payload1(i):
+    payload1 = {
+        'typ': ['stud'] * 12,
+        'numtxt': i,
+        'sbm': ['Y'] * 12
+    }
+    return payload1
 
 TOTAL = 8385
 
-def process_response_soup(soup, c):
-    for link in soup.select('.TableText a'):
-        roll = link.get_text().strip()
-        payload1['numtxt'] = roll
-        r1 = s.post("https://oa.cc.iitk.ac.in/Oa/Jsp/OAServices/IITk_SrchRes_new.jsp", headers=headers1, data=payload1)
-        soup1 = BeautifulSoup(r1.text, 'html.parser')
-
+def process_response_soup(soup1,roll,c):
         name = ''
         program = ''
         dept = ''
@@ -97,21 +88,53 @@ def process_response_soup(soup, c):
                    blood_group, gender, hometown))
 
 
-r = s.post("https://oa.cc.iitk.ac.in/Oa/Jsp/OAServices/IITk_SrchStudRoll_new.jsp", headers=headers, data=payload)
-soup = BeautifulSoup(r.text, 'html.parser')
-for link in soup.select('.DivContent'):
-    substituted = re.sub(r'\s+', ' ', link.text)
-    pattern = re.compile(r'\s*You are viewing 1 to 12 records out of (\d+) records\s*')
-    match = pattern.match(substituted)
-    TOTAL = int(match.group(1))
-    print("Total: {}".format(TOTAL))
-process_response_soup(soup, c)
-print("Processed 12")
-for i in range(12, TOTAL+1, 12):
-    payload['recpos'] = i
-    r = s.post("https://oa.cc.iitk.ac.in/Oa/Jsp/OAServices/IITk_SrchStudRoll_new.jsp", headers=headers, data=payload)
-    soup = BeautifulSoup(r.text, 'html.parser')
-    process_response_soup(soup, c)
-    print("Processed {}".format(i + 12))
-    conn.commit()
-conn.close()
+# r = s.post("https://oa.cc.iitk.ac.in/Oa/Jsp/OAServices/IITk_SrchStudRoll_new.jsp", headers=headers, data=payload)
+# soup = BeautifulSoup(r.text, 'html.parser')
+# for link in soup.select('.DivContent'):
+#     substituted = re.sub(r'\s+', ' ', link.text)
+#     pattern = re.compile(r'\s*You are viewing 1 to 12 records out of (\d+) records\s*')
+#     match = pattern.match(substituted)
+#     TOTAL = int(match.group(1))
+#     print("Total: {}".format(TOTAL))
+# process_response_soup(soup, c)
+# print("Processed 12")
+async def get_rolls():    
+    roll_list = []
+    async with aiohttp.ClientSession(trust_env = True) as session:
+        await session.get("https://oa.cc.iitk.ac.in/Oa/Jsp/Main_Frameset.jsp")
+        await session.get("https://oa.cc.iitk.ac.in/Oa/Jsp/Main_Intro.jsp?frm='SRCH'")
+        await session.get("https://oa.cc.iitk.ac.in/Oa/Jsp/OAServices/IITK_Srch.jsp?typ=stud")
+        pages = []
+        TOTAL = 100
+        payloads = list(map(get_payload,[i for i in range(0,TOTAL+1,12)]))
+        for payload in payloads:
+            pages.append(asyncio.create_task(session.post("https://oa.cc.iitk.ac.in/Oa/Jsp/OAServices/IITk_SrchStudRoll_new.jsp",data=payload,headers=headers, ssl = False)))
+        responses = await asyncio.gather(*pages)
+        for res in responses:
+            soup = BeautifulSoup(await res.text(), 'html.parser')
+            for link in soup.select('.TableText a'):
+                roll_list.append(link.get_text().strip())
+        print(roll_list)
+        return roll_list
+    # conn.commit()
+# conn.close()
+async def get_individual():
+    roll_list = await get_rolls()
+    async with aiohttp.ClientSession(trust_env = True) as session:
+        await session.get("https://oa.cc.iitk.ac.in/Oa/Jsp/Main_Frameset.jsp")
+        await session.get("https://oa.cc.iitk.ac.in/Oa/Jsp/Main_Intro.jsp?frm='SRCH'")
+        await session.get("https://oa.cc.iitk.ac.in/Oa/Jsp/OAServices/IITK_Srch.jsp?typ=stud")
+        payload1s = list(map(get_payload1,roll_list))
+        pages = []
+        for payload1 in payload1s:
+            pages.append(asyncio.create_task(session.post("https://oa.cc.iitk.ac.in/Oa/Jsp/OAServices/IITk_SrchRes_new.jsp", headers=headers1, data=payload1,ssl=False)))
+        responses = await(asyncio.gather(*pages))
+        i = 0
+        for res in responses:
+            roll = roll_list[i]
+            soup1 = BeautifulSoup(await res.text(), 'html.parser')
+            process_response_soup(soup1,roll,c)
+            i+=1
+        conn.commit()
+
+asyncio.run(get_individual())
